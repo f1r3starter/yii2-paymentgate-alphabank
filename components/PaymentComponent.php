2af40eb0627497ed9dfcd99d69c584c1abd93e3f
Ignatenkov Nikita <ignatenkov.n@gmail.com>
2016-12-13 18:31:45 +0300

    small refactoring

--- components/PaymentComponent.php
+++ components/PaymentComponent.php
@@ -5,66 +5,50 @@ use Yii;
 use yii\base\Component;
 use yii\base\InvalidConfigException;
 
+use yii\helpers\ArrayHelper;
 use yii\helpers\Url;
 
 class PaymentComponent extends Component
 {
     public $userName;
     public $password;
-    
+
     public $gatewayUrl;
-    
+
     public $returnUrl;
     public $failUrl;
-    
+
     public $errors = [];
-    
+
     private $_bankOrderId;
     private $_bankFormUrl;
-    
+
     /**
      * список статусов для заказов
+     * @param bool $status
+     * @return array|mixed
      */
-    public static function getOrderStatuses()
+    public static function getOrderStatuses($status = false)
     {
-        return [
-            [
-                'id' => 0,
-                'description' => 'Заказ зарегистрирован, но не оплачен.'
-            ],
-            [
-                'id' => 1,
-                'description' => 'Предавторизованная сумма захолдирована (для двухстадийных платежей).'
-            ],
-            [
-                'id' => 2,
-                'description' => 'Проведена полная авторизация суммы заказа.'
-            ],
-            [
-                'id' => 3,
-                'description' => 'Авторизация отменена.'
-            ],
-            [
-                'id' => 4,
-                'description' => 'По транзакции была проведена операция возврата.'
-            ],
-            [
-                'id' => 5,
-                'description' => 'Инициирована авторизация через ACS банка-эмитента.'
-            ],
-            [
-                'id' => 6,
-                'description' => 'Авторизация отклонена.'
-            ]
+        $statuses = [
+            0 => 'Заказ зарегистрирован, но не оплачен.',
+            1 => 'Предавторизованная сумма захолдирована (для двухстадийных платежей).',
+            2 => 'Проведена полная авторизация суммы заказа.',
+            3 => 'Авторизация отменена.',
+            4 => 'По транзакции была проведена операция возврата.',
+            5 => 'Инициирована авторизация через ACS банка-эмитента.',
+            6 => 'Авторизация отклонена.'
+
         ];
+        return $status !== false ? ArrayHelper::getValue($statuses, $status) : $statuses;
     }
-    
+
     /**
      * инициализация платежа в платёжном шлюзе
-     * 
+     *
      * должны получить id заказа в системе Альфа-Банка
      * и url-адрес формы для реквизитов банк. карты
-     * 
+     *
      *
      * ЗАПРОС РЕГИСТРАЦИИ ОДНОСТАДИЙНОГО ПЛАТЕЖА В ПЛАТЕЖНОМ ШЛЮЗЕ
      *      register.do
@@ -93,60 +77,60 @@ class PaymentComponent extends Component
      *      5           Ошибка значения параметра запроса.
      *      7           Системная ошибка.
      */
-    public function initPayment( $systemOrderId, $amount, $description )
+    public function initPayment($systemOrderId, $amount, $description)
     {
-        if( is_null($this->returnUrl) ) return false;
-        
+        if (is_null($this->returnUrl)) return false;
+
         $data = [
             'userName' => $this->userName,
             'password' => $this->password,
             'orderNumber' => urlencode($systemOrderId),
             'amount' => urlencode($amount),
-            'returnUrl' => Url::home(true).$this->returnUrl,
+            'returnUrl' => Url::home(true) . $this->returnUrl,
         ];
-        if( !is_null( $this->failUrl ) ) $data['failUrl'] = $this->failUrl;
-        
+        if (!is_null($this->failUrl)) $data['failUrl'] = $this->failUrl;
+
         $response = $this->gateway('register.do', $data);
-        
+
         // В случае ошибки вывести ее
-        if( isset($response['errorCode']) && ($response['errorCode'] > 0) ) { 
-        
+        if (isset($response['errorCode']) && ($response['errorCode'] > 0)) {
+
             $this->addError('Error #' . $response['errorCode'] . ': ' . (isset($response['errorMessage'])) ? $response['errorMessage'] : '');
-        
-        // В случае успеха сохранить номер заказа и formUrl, вернуть true
+
+            // В случае успеха сохранить номер заказа и formUrl, вернуть true
         } else {
-            
-            if( !isset($response['orderId']) || !isset($response['formUrl']) ) return false;
-            
+
+            if (!isset($response['orderId']) || !isset($response['formUrl'])) return false;
+
             $this->_bankOrderId = $response['orderId'];
             $this->_bankFormUrl = $response['formUrl'];
-            
+
             return true;
-        
+
         }
-        
+
         return false;
     }
-    
+
     /**
      * перенаправить пользователя на платёжную форму Альфа-Банка
      */
-    public function makePayment( $orderId )
+    public function makePayment($orderId)
     {
         return false;
     }
-    
+
     /**
-     * получить статус платежа 
+     * получить статус платежа
      */
-    public function getStatus( $orderId )
+    public function getStatus($orderId)
     {
         $data = array(
             'userName' => $this->userName,
             'password' => $this->password,
             'orderId' => $orderId
         );
-         
+
         /**
          * ЗАПРОС СОСТОЯНИЯ ЗАКАЗА
          *      getOrderStatus.do
@@ -180,23 +164,23 @@ class PaymentComponent extends Component
          *      6           Авторизация отклонена.
          */
         $response = $this->gateway('getOrderStatus.do', $data);
-        
+
         // В случае ошибки вывести ее
-        if( isset($response['errorCode']) && ($response['errorCode'] > 0) ) { 
-        
+        if (isset($response['errorCode']) && ($response['errorCode'] > 0)) {
+
             $this->addError('Error #' . $response['errorCode'] . ': ' . (isset($response['errorMessage'])) ? $response['errorMessage'] : '');
-        
-        // В случае успеха вернуть номер статуса заказа в системе Альфа-Банка
+
+            // В случае успеха вернуть номер статуса заказа в системе Альфа-Банка
         } else {
-            
-            if( !isset($response['OrderStatus']) ) return false;
+
+            if (!isset($response['OrderStatus'])) return false;
             return $response['OrderStatus'];
-        
+
         }
-        
+
         return false;
     }
-    
+
     /**
      * получить id заказа в системе Альфа-Банка
      */
@@ -204,7 +188,7 @@ class PaymentComponent extends Component
     {
         return $this->_bankOrderId;
     }
-    
+
     /**
      * получить адрес формы Альфа-Банка для ввода реквизитов безналичной оплаты
      */
@@ -212,20 +196,21 @@ class PaymentComponent extends Component
     {
         return $this->_bankFormUrl;
     }
-    
+
     /**
      * сформировать адрес страницы для просмотра статуса платежа
      */
-    public function formStatusUrl( $orderId )
+    public function formStatusUrl($orderId)
     {
-        return Url::home(true) . Url::to([ $this->returnUrl, 'orderId' => $orderId ]);
+        return Url::home(true) . Url::to([$this->returnUrl, 'orderId' => $orderId]);
     }
-    
+
     /**
      * отправка запроса на платёжный шлюз через Curl (метод POST)
      */
     private function gateway($method, $data)
     {
+
         $curl = curl_init(); // Инициализируем запрос
         curl_setopt_array($curl, array(
             CURLOPT_URL => $this->gatewayUrl . $method, // Полный адрес метода
@@ -234,16 +219,16 @@ class PaymentComponent extends Component
             CURLOPT_POSTFIELDS => http_build_query($data) // Данные в запросе
         ));
         $response = curl_exec($curl); // Выполненяем запрос
-        
+
         $response = json_decode($response, true); // Декодируем из JSON в массив
         curl_close($curl); // Закрываем соединение
         return $response; // Возвращаем ответ
     }
-    
+
     /**
      * добавить сообщение об ошибке в массив сообщений об ошибках
      */
-    private function addError( $message )
+    private function addError($message)
     {
         $this->errors[] = $message;
         return true;
--- controllers/PaymentController.php
+++ controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace paymentgate_alphabank\controllers;
 
+use paymentgate_alphabank\components\PaymentComponent;
 use Yii;
 
 use yii\web\Controller;
@@ -52,36 +53,33 @@ class PaymentController extends Controller
     {
         Yii::$app->getModule('wallets');
         Yii::$app->getModule('queue');
-        
+
         return parent::beforeAction($action);
     }
-    
+
     /**
+     * @param $orderId
      * @return mixed
+     * @throws NotFoundHttpException
      */
-    public function actionReturn( $orderId )
+    public function actionReturn($orderId)
     {
         $paymentGate = PaymentGateAlphaBank::getInstance();
-        
+
         $paymentClass = $paymentGate->paymentClass;
         // здесь надо поставить Exception, если нет $paymentClass
-        
+
         $payment = $paymentClass::find()->where([$paymentGate->paymentOrderField => $orderId])->one();
-        if( empty($payment) ) throw new NotFoundHttpException( Yii::t('paymentgate_alphabank','The requested page does not exist.') );
-        
-        $orderStatus = Yii::$app->paymentgate_alphabank->getStatus( $orderId );
-        $orderList = ArrayHelper::map( Yii::$app->paymentgate_alphabank->getOrderStatuses(), 'id', 'description' );
-        
-        if( !empty($orderStatus) && array_key_exists($orderStatus, $orderList) ) {
-            $orderStatusDescription = $orderList[ $orderStatus ];
-        }
-        
+        if (empty($payment)) throw new NotFoundHttpException(Yii::t('paymentgate_alphabank', 'The requested page does not exist.'));
+
+        $orderStatus = Yii::$app->paymentgate_alphabank->getStatus($orderId);
+
         return $this->render('return', [
             'paymentGate' => $paymentGate,
             'payment' => $payment,
-            'orderStatusDescription' => ( isset($orderStatusDescription) ) ? $orderStatusDescription : null,
-        ]);;
+            'orderStatusDescription' => PaymentComponent::getOrderStatuses($orderStatus),
+        ]);
     }
-    
-    
+
+
 }
